bd1e5ed2832dfc3dee9666103a56dfa99e325cc9
Farhan Rasyid Mustaqim <farhanID0105@gmail.com>
2025-05-27 16:02:29 +0700

    Membuat class pasien utk member yg bisa di akses di public

--- Asosiasi/Association.cpp
+++ Asosiasi/Association.cpp
@@ -2,5 +2,24 @@
 #include <vector>
 using namespace std;
 
+class dokter;
+
+class pasien {      //Assosiasi
+    public:
+        string nama;
+        vector<dokter*> daftar_doktor;
+        pasien(string pNama): nama(pNama) {
+            cout << "Pasien \"" << nama << "\" ada\n";
+        }
+
+        ~pasien() {
+            cout << "Pasien \"" << nama << "\" tidak ada\n";
+        }
+
+        void tambahDokter(dokter*);
+        void cetakDokter();
+};
+
+
 
 
